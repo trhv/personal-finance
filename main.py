8e744e14df2881b1d2eca22a5389e7580624580d
arye shustimov <arye_s@optimove.com>
2024-03-26 21:18:21 +0200

    add package

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from services.bank_service import BankService
-from services.whatsup_service import WhatsupService
+# from services.bank_service import BankService
+# from services.whatsup_service import WhatsupService
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+# print(f"Database Host: {os.getenv("BANK_PASS")}")
 # bank = BankService()
 # bank.connect()
 # bank.scrap()
 
 
-whatsUp = WhatsupService()
-whatsUp.send()
+# whatsUp = WhatsupService()
+# whatsUp.send()
